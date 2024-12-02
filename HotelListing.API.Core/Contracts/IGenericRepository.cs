3937ad3b9cc62c6ffd9cbc31f9b3156c243e91b3
Sandić Nenad <sandicnenad81@gmail.com>
2024-12-02 08:28:43 +0100

    Section_10: Repository refactor

--- HotelListing.API.Core/Contracts/IGenericRepository.cs
+++ HotelListing.API.Core/Contracts/IGenericRepository.cs
@@ -6,16 +6,14 @@ namespace HotelListing.API.Core.Contracts
     {
         Task<T> GetAsync(int? id);
         Task<TResult> GetAsync<TResult>(int? id);
-
         Task<List<T>> GetAllAsync();
+        Task<List<TResult>> GetAllAsync<TResult>();
         Task<PagedResult<TResult>> GetAllAsync<TResult>(QueryParameters queryParameters);
-
         Task<T> AddAsync(T entity);
-
+        Task<TResult> AddAsync<Tsource, TResult>(Tsource source);
         Task DeleteAsync(int id);
-
         Task UpdateAsync(T entity);
-
+        Task UpdateAsync<TSource>(int id, TSource source);
         Task<bool> Exists(int id);
     }
 }
--- HotelListing.API.Core/Repository/GenericRepository.cs
+++ HotelListing.API.Core/Repository/GenericRepository.cs
@@ -4,6 +4,7 @@ using HotelListing.API.Core.Contracts;
 using HotelListing.API.Data;
 using HotelListing.API.Core.Models;
 using Microsoft.EntityFrameworkCore;
+using HotelListing.API.Core.Exceptions;
 
 namespace HotelListing.API.Core.Repository
 {
@@ -24,9 +25,23 @@ namespace HotelListing.API.Core.Repository
             return entity;
         }
 
+        public async Task<TResult> AddAsync<Tsource, TResult>(Tsource source)
+        {
+            var entity = _mapper.Map<T>(source);
+
+            await _context.AddAsync(entity);
+            await _context.SaveChangesAsync();
+
+            return _mapper.Map<TResult>(entity);
+        }
+
         public async Task DeleteAsync(int id)
         {
             var entity = await GetAsync(id);
+
+            if (entity is null)
+                throw new NotFoundException(typeof(T).Name, id);
+
             _context.Set<T>().Remove(entity);
             await _context.SaveChangesAsync();
         }
@@ -50,6 +65,7 @@ namespace HotelListing.API.Core.Repository
                 .Take(queryParameters.PageSize)
                 .ProjectTo<TResult>(_mapper.ConfigurationProvider)
                 .ToListAsync();
+
             return new PagedResult<TResult>
             {
                 Items = items,
@@ -57,6 +73,13 @@ namespace HotelListing.API.Core.Repository
                 RecordNumber = queryParameters.PageSize,
                 TotalCount = totalSize
             };
+        }
+
+        public async Task<List<TResult>> GetAllAsync<TResult>()
+        {
+            return await _context.Set<T>()
+                .ProjectTo<TResult>(_mapper.ConfigurationProvider)
+                .ToListAsync();
         }
 
         public async Task<T> GetAsync(int? id)
@@ -71,17 +94,31 @@ namespace HotelListing.API.Core.Repository
 
         public async Task<TResult> GetAsync<TResult>(int? id)
         {
-            if(id is null)
-            {
-                return default;
-            }
-
             var result = await _context.Set<T>().FindAsync(id);
+            if (result is null)
+            {
+                throw new NotFoundException(typeof(T).Name, id.HasValue ? id : "No Key Provided");
+            }
+            
             return _mapper.Map<TResult>(result);
         }
 
         public async Task UpdateAsync(T entity)
         {
+            _context.Update(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync<TSource>(int id, TSource source)
+        {
+            var entity = await GetAsync(id);
+
+            if (entity is null)
+            {
+                throw new NotFoundException(typeof(T).Name, id);
+            }
+
+            _mapper.Map(source, entity);
             _context.Update(entity);
             await _context.SaveChangesAsync();
         }
